8f1eeda67ed156b93199f9286023bd562199fd9d
Andreia Cabral da Costa <81486763+andreiacabral@users.noreply.github.com>
2021-12-22 22:27:02 -0300

    Subida de exercicios

---
+++ 1154/URI1154/URI1154/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Globalization;
+double idade = 0, soma = 0, cont = 0;
+double media=0;
+
+idade = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+
+while (idade >=0)
+{
+    soma = soma + idade;
+    cont = cont + 1;
+    idade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+}
+media = soma / cont;
+Console.WriteLine(media.ToString("F2", CultureInfo.InvariantCulture));
+Console.WriteLine();
---
+++ 1159/URI1159/URI1159/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+int X,resultado;
+int ePar;
+
+X=int.Parse(Console.ReadLine());
+
+while (X!=0)
+{
+    ePar = X %2;
+
+    if (ePar == 0)
+    {
+        resultado = X;
+
+        for (int i = 0; i <= 3; i++)
+        {
+            X += 2;
+            resultado += X;
+
+        }
+
+    } 
+    else
+    {
+        resultado = ++X;
+
+        for (int i = 0; i <= 3; i++)
+        {
+            X += 2;
+            resultado += X;
+
+        }
+
+    }
+
+    Console.WriteLine(resultado);
+    X=int.Parse (Console.ReadLine());
+}
+
