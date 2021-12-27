b357daf525ffe99fcd5cb24f2895a93a65cba4de
Andreia Cabral da Costa <81486763+andreiacabral@users.noreply.github.com>
2021-12-27 12:09:04 -0300

    URI1131

---
+++ 1118/URI1118/URI1118/Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Globalization;
+
+int repeteCalculo = 1;
+double nota1 = 0, nota2 = 0, media;
+bool nota1Invalida = true, nota2Invalida = true, repeteInvalido = true;
+
+while (repeteCalculo == 1)
+{
+    while (nota1Invalida)
+    {
+        nota1 = double.Parse(Console.ReadLine());
+
+        if (nota1 <= 10 && nota1 >= 0)
+        {
+            nota1Invalida = false;
+        }
+        else
+        {
+            Console.WriteLine("nota invalida");
+        }
+    }
+
+    while (nota2Invalida)
+    {
+        nota2 = double.Parse(Console.ReadLine());
+
+        if (nota2 <= 10 && nota2 >= 0)
+        {
+            nota2Invalida = false;
+        }
+        else
+        {
+            Console.WriteLine("nota invalida");
+        }
+    }
+
+    media = (nota1 + nota2) / 2;
+
+    Console.WriteLine("media = " + media.ToString("F2", CultureInfo.InvariantCulture));
+    while (repeteInvalido)
+    {
+        Console.WriteLine("novo calculo (1-sim 2-nao)");
+        repeteCalculo = int.Parse(Console.ReadLine());
+
+        if (repeteCalculo == 1 || repeteCalculo == 2)
+        {
+            repeteInvalido = false;
+        }
+
+    }
+
+    nota1Invalida = true;
+    nota2Invalida = true;
+    repeteInvalido = true;
+}
+
---
+++ 1131/URI1131/URI1131/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace URI1131
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            int golsInter, golsGremio, grenais = 0,empate=0, repete = 1, vitoriasInter=0, vitoriasGremio=0; ;
+            string[] entradaGols;
+
+            while (repete == 1)
+            {
+                grenais++;
+                entradaGols = Console.ReadLine().Split(' ');
+
+                golsInter = int.Parse(entradaGols[0]);
+                golsGremio = int.Parse(entradaGols[1]);
+
+                if (golsGremio>golsInter)
+                {
+                    vitoriasGremio++;
+                }
+                else if (golsGremio < golsInter)
+                {
+                    vitoriasInter++;
+                }
+                else
+                {
+                    empate++; 
+                }
+
+
+                Console.WriteLine("Novo grenal (1-sim 2-nao)");
+                repete = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(grenais+ " grenais");
+            Console.WriteLine("Inter:" + vitoriasInter);
+            Console.WriteLine("Gremio:" + vitoriasGremio);
+            Console.WriteLine("empate:" + empate);
+
+            if (vitoriasGremio>vitoriasInter)
+            {
+                Console.WriteLine("Gremio venceu mais");
+            }
+            else if (vitoriasGremio < vitoriasInter)
+            {
+                Console.WriteLine("Inter venceu mais");
+            }
+            else
+            {
+                Console.WriteLine("Nao houve vencedor");
+            }
+         
+
+
+        }
+    }
+}
