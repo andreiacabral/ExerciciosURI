9deaa0dadf6945fdb3c804a7e947a5ad9bee17ba
Andreia Cabral da Costa <81486763+andreiacabral@users.noreply.github.com>
2022-01-04 20:38:25 -0300

    URI1080

---
+++ 1079/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+int MaiorValor=0, posicao=0,valor;
+
+for (int i = 0; i < 10; i++)
+{
+    valor=int.Parse(Console.ReadLine());
+
+    if (valor>MaiorValor)
+    {
+        MaiorValor = valor;
+        posicao = i;
+    }
+}
+Console.WriteLine(MaiorValor);
+Console.WriteLine(posicao);
